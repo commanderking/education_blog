f3eafc0ff2e5f089d3e0f87879b31d55b0e4524c
commanderking <yaoguais@Jeffreys-MacBook-Pro.local>
2020-06-25 21:31:12 -0400

    add prototype page and import open-math-tools

---
+++ src/pages/prototypes.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Prototypes = () => {
+  return (
+    <div>
+      <Layout>
+        <h3>Prototypes</h3>
+      </Layout>
+    </div>
+  );
+};
+
+export default Prototypes;
