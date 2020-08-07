6c043b26d9ae1ba9a5fdaec32e419c421b06977b
Jeffrey King <jek582@mail.harvard.edu>
2020-08-07 17:01:21 -0400

    center subscribe for desktop and mobile

--- src/components/subscribeForm.tsx
+++ src/components/subscribeForm.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const SubscribeForm = () => {
   return (
-    <form action="https://tinyletter.com/commanderking" method="post">
+    <form
+      action="https://tinyletter.com/commanderking"
+      method="post"
+      style={{ textAlign: "center" }}
+    >
       <div style={{ paddingBottom: "8px" }}>
         Sign up for the monthly newsletter:
       </div>
       <input
-        style={{ padding: "8px", width: "275px" }}
+        style={{ padding: "8px", width: "275px", marginTop: "5px" }}
         placeholder="youremail@email.com"
         type="text"
         name="email"
@@ -20,6 +24,7 @@ const SubscribeForm = () => {
         style={{
           marginLeft: "2px",
           padding: "8px 12px 8px 12px",
+          marginTop: "5px",
         }}
       />
     </form>
