4dc0566612d2693f7a2737ec5af3f1f5f39da75e
Jeffrey King <jek582@mail.harvard.edu>
2020-08-11 22:58:45 -0400

    updates to blog post

--- content/blog/cell-tower-2/prototype1.tsx
+++ content/blog/cell-tower-2/prototype1.tsx
@@ -2,38 +2,44 @@ import React from "react";
 import { CoordinateGrid } from "open-math-tools";
 import cellTower from "../../assets/cell-tower.svg";
 import homeIcon from "../..//assets/home-icon.svg";
+import PrototypeWrapper from "../../../src/components/prototypeWrapper.tsx";
 
 const houseCoordinates = [
-  { x: 5, y: 9 },
   { x: -5, y: -2 },
   { x: -3, y: -4 },
   { x: 1, y: -3 },
-  { x: 3, y: -2 },
-  { x: 5, y: 4 },
-  { x: 1, y: 8 },
-  { x: -2, y: 7 },
-  { x: 9, y: -9 },
 ];
 
 const PrototypeOne = () => {
   return (
-    <CoordinateGrid
-      id="CellTowerPrototype"
-      gridHeight={300}
-      gridWidth={300}
-      addableIcon={{
-        iconImage: cellTower,
-        iconSize: 20,
-        maxIcons: 5,
-      }}
-      preplacedIcons={[
-        {
-          iconImage: homeIcon,
-          iconSize: 15,
-          coordinates: houseCoordinates,
-        },
-      ]}
-    />
+    <div style={{ marginBottom: "20px" }}>
+      <PrototypeWrapper title="Prototype 1">
+        <CoordinateGrid
+          id="CellTowerPrototype"
+          gridHeight={300}
+          gridWidth={300}
+          addableIcon={{
+            iconImage: cellTower,
+            iconSize: 20,
+            maxIcons: 5,
+          }}
+          xTicksNumber={10}
+          yTicksNumber={10}
+          preplacedIcons={[
+            {
+              iconImage: homeIcon,
+              iconSize: 12,
+              coordinates: houseCoordinates,
+            },
+          ]}
+        />
+        <div>
+          Houses within 4 kilometers of a tower can get service. Which of the
+          households will get service based on the cell tower's current
+          location?
+        </div>
+      </PrototypeWrapper>
+    </div>
   );
 };
 
